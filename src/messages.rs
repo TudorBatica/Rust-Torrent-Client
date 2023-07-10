58ca47147317e4ab3d40d7ca44adc5768b4ed00f
tbatica <tudor.batica@gmail.com>
2023-07-10 21:33:31 +0300

    implement peer handshake

---
+++ src/messages.rs
@@ -0,0 +1,23 @@
+const PROTOCOL: &'static str = "BitTorrent protocol";
+
+pub struct Handshake {
+    pub content: Vec<u8>,
+}
+
+impl Handshake {
+    pub fn new(info_hash: &Vec<u8>, client_id: &str) -> Self {
+        let mut content: Vec<u8> = Vec::with_capacity(49 + PROTOCOL.len());
+        //pstrlen
+        content.push(PROTOCOL.len() as u8);
+        //pstr
+        content.extend(PROTOCOL.bytes());
+        //reserved bytes
+        content.extend(vec![0; 8].into_iter());
+        //info hash of desired torrent
+        content.extend(info_hash);
+        //client id
+        content.extend(client_id.bytes());
+
+        return Handshake { content };
+    }
+}
--- src/tracker.rs
+++ src/tracker.rs
@@ -13,24 +13,16 @@ pub struct Peer {
 
 #[derive(Debug, Deserialize)]
 pub struct TrackerResponse {
+    #[serde(default)]
+    pub complete: u16,
+    #[serde(default)]
+    pub incomplete: u16,
     pub interval: u64,
     #[serde(deserialize_with = "deserialize_peers")]
     pub peers: Vec<Peer>,
-}
-
-fn deserialize_peers<'de, D>(deserializer: D) -> Result<Vec<Peer>, D::Error>
-    where D: serde::Deserializer<'de>,
-{
-    let bytes = serde_bytes::ByteBuf::deserialize(deserializer)?;
-    let mut iter = bytes.as_ref().chunks_exact(6);
-    let mut peers = Vec::new();
-    while let Some(chunk) = iter.next() {
-        let ip = Ipv4Addr::new(chunk[0], chunk[1], chunk[2], chunk[3]);
-        let port = u16::from_be_bytes([chunk[4], chunk[5]]);
-        peers.push(Peer { ip, port });
-    }
-
-    Ok(peers)
+    #[serde(default)]
+    #[serde(rename = "tracker id")]
+    pub tracker_id: Option<String>,
 }
 
 pub async fn announce(torrent: &Torrent, peer_config: &Config) -> Result<TrackerResponse, Box<dyn std::error::Error>> {
@@ -45,7 +37,7 @@ fn create_url(torrent: &Torrent, peer_config: &Config) -> String {
     let info_hash = byte_serialize(&torrent.info_hash).collect::<String>();
     let query_params = [
         ("info_hash", info_hash.as_ref()),
-        ("peer_id", peer_config.peer_id.as_ref()),
+        ("peer_id", peer_config.client_id.as_ref()),
         ("port", port.as_ref()),
         ("compact", "1"),
         ("event", "started"),
@@ -63,4 +55,19 @@ fn create_url(torrent: &Torrent, peer_config: &Config) -> String {
         .collect::<String>();
 
     return format!("{}?{}", torrent.announce, query_params);
+}
+
+fn deserialize_peers<'de, D>(deserializer: D) -> Result<Vec<Peer>, D::Error>
+    where D: serde::Deserializer<'de>,
+{
+    let bytes = serde_bytes::ByteBuf::deserialize(deserializer)?;
+    let mut iter = bytes.as_ref().chunks_exact(6);
+    let mut peers = Vec::new();
+    while let Some(chunk) = iter.next() {
+        let ip = Ipv4Addr::new(chunk[0], chunk[1], chunk[2], chunk[3]);
+        let port = u16::from_be_bytes([chunk[4], chunk[5]]);
+        peers.push(Peer { ip, port });
+    }
+
+    Ok(peers)
 }
