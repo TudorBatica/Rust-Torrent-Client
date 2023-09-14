62b14503ac67d328de56824e6e6d01f6eff119ab
tbatica <tudor.batica@gmail.com>
2023-09-14 13:33:37 +0300

    fix piece picker pick method and implement reinsert piece method

--- src/piece_picker.rs
+++ src/piece_picker.rs
@@ -1,4 +1,5 @@
 use std::collections::{HashMap, HashSet};
+use std::hint::black_box;
 use rand::prelude::IteratorRandom;
 use crate::core_models::entities::{Block, TorrentLayout};
 use crate::core_models::entities::Bitfield;
@@ -6,6 +7,7 @@ use async_trait::async_trait;
 use mockall::automock;
 
 const ALL_BLOCKS_IN_TRANSFER_PENALTY: i32 = 100;
+const ALL_BLOCKS_REMOVED_PENALTY: i32 = 50000;
 
 pub struct PickResult {
     pub blocks: Vec<Block>,
@@ -35,6 +37,10 @@ impl PieceDownloadState {
 
         return piece;
     }
+
+    fn all_blocks_removed(&self) -> bool {
+        return self.blocks_in_transfer.is_empty() && self.blocks_unrequested.is_empty();
+    }
 }
 
 #[automock]
@@ -48,6 +54,7 @@ pub trait PiecePicker: Send {
 }
 
 pub struct RarestPiecePicker {
+    layout: TorrentLayout,
     // vec of (piece_idx, inverse_priority)
     priority_sorted_pieces: Vec<(usize, i32)>,
     // key: piece idx, value: index of the piece in the priority sorted pieces vector
@@ -58,7 +65,7 @@ pub struct RarestPiecePicker {
 }
 
 impl RarestPiecePicker {
-    pub fn init(layout: &TorrentLayout) -> Self {
+    pub fn init(layout: TorrentLayout) -> Self {
         let priority_sorted_pieces: Vec<(usize, i32)> = (0..layout.pieces).map(|idx| (idx, 0)).collect();
         let piece_lookup_table: HashMap<usize, usize> = (0..layout.pieces).map(|idx| (idx, idx)).collect();
         let mut piece_download_state: HashMap<usize, PieceDownloadState> = HashMap::new();
@@ -67,12 +74,20 @@ impl RarestPiecePicker {
             piece_download_state.insert(piece_idx, piece);
         }
 
-        return RarestPiecePicker { priority_sorted_pieces, piece_lookup_table, piece_download_state };
+        return RarestPiecePicker { layout, priority_sorted_pieces, piece_lookup_table, piece_download_state };
     }
 
     fn pick_blocks(&mut self, piece_idx: usize, num_of_blocks: usize) -> PickResult {
         let piece = self.piece_download_state.get_mut(&piece_idx).unwrap();
-        let blocks: Vec<(usize, usize)> = piece.blocks_unrequested.drain().take(num_of_blocks).collect();
+
+        let blocks: Vec<(usize, usize)> = piece.blocks_unrequested.iter()
+            .take(num_of_blocks)
+            .map(|(offset, length)| (*offset, *length))
+            .collect();
+        blocks.iter().for_each(|block| {
+            piece.blocks_unrequested.remove(block);
+        });
+
         if !blocks.is_empty() {
             piece.blocks_in_transfer.extend(blocks.clone());
             return PickResult {
@@ -81,7 +96,7 @@ impl RarestPiecePicker {
                 end_game_mode_enabled: false,
             };
         }
-        // End Game scenario, at most one block will be picked at random
+        // End Game scenario, at most one in-transfer block will be picked at random
         let blocks: Vec<Block> = piece.blocks_in_transfer.iter()
             .choose(&mut rand::thread_rng())
             .map_or_else(|| vec![],
@@ -130,7 +145,9 @@ impl PiecePicker for RarestPiecePicker {
     fn pick(&mut self, peer_bitfield: &Bitfield, num_of_blocks: usize) -> Option<PickResult> {
         // find the first piece owned by the peer
         let (piece_idx, _) = self.priority_sorted_pieces.iter()
-            .find(|(piece_idx, _)| peer_bitfield.has_piece(*piece_idx))
+            .find(|(piece_idx, score)| {
+                score < &ALL_BLOCKS_REMOVED_PENALTY && peer_bitfield.has_piece(*piece_idx)
+            })
             ?.to_owned();
 
         // pick blocks from it
@@ -162,17 +179,18 @@ impl PiecePicker for RarestPiecePicker {
     }
 
     fn remove_block(&mut self, block: &Block) {
-        //todo: fix
-        if let Some(piece_state) = self.piece_download_state.get_mut(&block.piece_idx) {
-            piece_state.blocks_in_transfer.remove(&(block.offset, block.length));
-            if let Some(array_idx) = self.piece_lookup_table.get(&block.piece_idx) {
-                self.priority_sorted_pieces.remove(*array_idx);
-            }
+        let piece_state = self.piece_download_state.get_mut(&block.piece_idx).unwrap();
+        piece_state.blocks_in_transfer.remove(&(block.offset, block.length));
+        if piece_state.all_blocks_removed() {
+            println!("all blocks removed!!!!");
+            self.update_priority(block.piece_idx, ALL_BLOCKS_REMOVED_PENALTY);
         }
     }
 
     fn reinsert_piece(&mut self, piece_idx: usize) {
-        //todo: implement
+        let fresh_state = PieceDownloadState::init(piece_idx, &self.layout);
+        self.piece_download_state.insert(piece_idx, fresh_state);
+        self.update_priority(piece_idx, -(ALL_BLOCKS_REMOVED_PENALTY + ALL_BLOCKS_IN_TRANSFER_PENALTY));
     }
 }
 
@@ -180,13 +198,13 @@ impl PiecePicker for RarestPiecePicker {
 #[cfg(test)]
 mod tests {
     use crate::piece_picker::{PiecePicker, RarestPiecePicker};
-    use crate::core_models::entities::Bitfield;
-    use crate::mocks;
+    use crate::core_models::entities::{Bitfield, Block};
+    use crate::{config, mocks};
 
     #[test]
     fn test_piece_pick_no_pieces_available() {
         let layout = mocks::generate_mock_layout(2, 2, 2);
-        let mut piece_picker = RarestPiecePicker::init(&layout);
+        let mut piece_picker = RarestPiecePicker::init(layout);
         let peer_bitfield = Bitfield::init(2);
         let pick_result = piece_picker.pick(&peer_bitfield, 2);
 
@@ -196,7 +214,7 @@ mod tests {
     #[test]
     fn test_piece_pick() {
         let layout = mocks::generate_mock_layout(2, 2, 2);
-        let mut piece_picker = RarestPiecePicker::init(&layout);
+        let mut piece_picker = RarestPiecePicker::init(layout);
         let mut peer_bitfield = Bitfield::init(2);
 
         peer_bitfield.piece_acquired(0);
@@ -211,7 +229,7 @@ mod tests {
     #[test]
     fn test_piece_pick_rarest_picked_first() {
         let layout = mocks::generate_mock_layout(2, 2, 2);
-        let mut piece_picker = RarestPiecePicker::init(&layout);
+        let mut piece_picker = RarestPiecePicker::init(layout);
         let mut peer_bitfield = Bitfield::init(2);
 
         peer_bitfield.piece_acquired(0);
@@ -227,7 +245,7 @@ mod tests {
     #[test]
     fn test_piece_pick_end_game_mode() {
         let layout = mocks::generate_mock_layout(2, 2, 2);
-        let mut piece_picker = RarestPiecePicker::init(&layout);
+        let mut piece_picker = RarestPiecePicker::init(layout);
         let mut peer_1 = Bitfield::init(2);
         let mut peer_2 = Bitfield::init(2);
 
@@ -241,5 +259,38 @@ mod tests {
 
         assert_eq!(pick_result.blocks.len(), 1);
         assert_eq!(pick_result.end_game_mode_enabled, true);
+    }
+
+    #[test]
+    fn test_remove_blocks_in_piece() {
+        let layout = mocks::generate_mock_layout(2, 2, 2);
+        let mut piece_picker = RarestPiecePicker::init(layout);
+        let mut peer = Bitfield::init(2);
+
+        peer.piece_acquired(0);
+        let pick = piece_picker.pick(&peer, 1).unwrap();
+        piece_picker.remove_block(&pick.blocks[0]);
+        let pick = piece_picker.pick(&peer, 2).unwrap();
+        assert_eq!(pick.blocks.len(), 1);
+        assert_eq!(pick.end_game_mode_enabled, false);
+        piece_picker.remove_block(&pick.blocks[0]);
+        let pick = piece_picker.pick(&peer, 2);
+        assert!(pick.is_none());
+    }
+
+    #[test]
+    fn test_reinsert_piece() {
+        let layout = mocks::generate_mock_layout(2, 2, 2);
+        let mut piece_picker = RarestPiecePicker::init(layout);
+        let mut peer = Bitfield::init(2);
+
+        peer.piece_acquired(0);
+        let pick = piece_picker.pick(&peer, 2).unwrap();
+        piece_picker.remove_block(&pick.blocks[0]);
+        piece_picker.remove_block(&pick.blocks[1]);
+
+        piece_picker.reinsert_piece(0);
+        let pick = piece_picker.pick(&peer, 2).unwrap();
+        assert_eq!(pick.blocks.len(), 2);
     }
 }
