f4cff507125b48d33bb79823a7e28ef9d5df5530
tbatica <tudor.batica@gmail.com>
2023-10-03 13:58:09 +0300

    update p2p handlers tests

--- src/p2p/handlers.rs
+++ src/p2p/handlers.rs
@@ -217,216 +217,169 @@ mod tests {
         assert!(result.messages_for_peer[0].is_not_interested());
     }
 
-    // #[tokio::test]
-    // async fn pick_blocks_test() {
-    //     let mut result = HandlerResult::new();
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.client_is_interested = true;
-    //     state.client_is_choked = false;
-    //     state.ongoing_requests = HashSet::new();
-    //     let pick_result = PickResult {
-    //         blocks: vec![Block::new(0, 0, 0)],
-    //         end_game_mode_enabled: false,
-    //     };
-    //     let mut pick_option = Some(pick_result.clone());
-    //     let mut picker = MockPiecePicker::new();
-    //     picker.expect_pick().returning(move |_, _| pick_option.take());
-    //     let picker = Arc::new(Mutex::new(picker));
-    //
-    //     pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
-    //
-    //     assert!(result.internal_events.is_empty());
-    //     assert_eq!(state.ongoing_requests.len(), pick_result.blocks.len());
-    //     assert_eq!(result.messages_for_peer.len(), pick_result.blocks.len());
-    //     assert!(result.messages_for_peer.iter().all(|msg| msg.is_request()));
-    // }
+    #[tokio::test]
+    async fn pick_blocks_test() {
+        let mut result = HandlerResult::new();
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        state.client_is_interested = true;
+        state.client_is_choked = false;
+        state.ongoing_requests = HashSet::new();
 
-    // #[tokio::test]
-    // async fn pick_blocks_when_end_game_picked_by_picker_test() {
-    //     let mut result = HandlerResult::new();
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.client_is_interested = true;
-    //     state.client_is_choked = false;
-    //     state.ongoing_requests = HashSet::new();
-    //     let pick_result = PickResult {
-    //         blocks: vec![Block::new(0, 0, 0)],
-    //         end_game_mode_enabled: true,
-    //     };
-    //     let mut pick_option = Some(pick_result.clone());
-    //     let mut picker = MockPiecePicker::new();
-    //     picker.expect_pick().returning(move |_, _| pick_option.take());
-    //     let picker = Arc::new(Mutex::new(picker));
-    //
-    //     pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
-    //
-    //     assert_eq!(result.internal_events.len(), 1);
-    //     assert!(result.internal_events[0].is_end_game_enabled());
-    //     assert_eq!(state.ongoing_requests.len(), pick_result.blocks.len());
-    //     assert_eq!(result.messages_for_peer.len(), pick_result.blocks.len());
-    //     assert!(result.messages_for_peer.iter().all(|msg| msg.is_request()));
-    //     assert_eq!(state.ongoing_requests.len(), pick_result.blocks.len());
-    //     assert!(state.endgame_enabled);
-    // }
-    //
-    // #[tokio::test]
-    // async fn pick_blocks_when_no_pieces_picked_by_picker_test() {
-    //     let mut result = HandlerResult::new();
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.client_is_interested = true;
-    //     state.client_is_choked = false;
-    //     let initial_state = state.clone();
-    //     let mut picker = MockPiecePicker::new();
-    //     picker.expect_pick().returning(move |_, _| None);
-    //     let picker = Arc::new(Mutex::new(picker));
-    //
-    //     pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
-    //
-    //     assert!(result.internal_events.is_empty());
-    //     assert!(result.messages_for_peer.is_empty());
-    //     assert_eq!(state, initial_state);
-    // }
+        let picked_blocks = vec![Block::new(0, 0, 0)];
+        let picked = picked_blocks.clone();
+        let mut picker = MockPiecePicker::new();
+        picker.expect_pick().returning(move |_, _| picked_blocks.clone());
+        let picker = Arc::new(Mutex::new(picker));
 
-    // #[tokio::test]
-    // async fn pick_blocks_when_client_choked_test() {
-    //     let mut result = HandlerResult::new();
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.client_is_choked = true;
-    //     state.client_is_interested = true;
-    //     let initial_state = state.clone();
-    //     let picker = Arc::new(Mutex::new(MockPiecePicker::new()));
-    //
-    //     pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
-    //
-    //     assert!(result.internal_events.is_empty());
-    //     assert!(result.messages_for_peer.is_empty());
-    //     assert_eq!(state, initial_state);
-    // }
-    //
-    // #[tokio::test]
-    // async fn pick_blocks_when_client_not_interested_test() {
-    //     let mut result = HandlerResult::new();
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.client_is_choked = false;
-    //     state.client_is_interested = false;
-    //     let initial_state = state.clone();
-    //     let picker = Arc::new(Mutex::new(MockPiecePicker::new()));
-    //
-    //     pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
-    //
-    //     assert!(result.internal_events.is_empty());
-    //     assert!(result.messages_for_peer.is_empty());
-    //     assert_eq!(state, initial_state);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_choke_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //     state.client_is_choked = false;
-    //
-    //     let msg = PeerMessage(Message::Choke);
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(state.client_is_choked);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_unchoke_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //     state.client_is_choked = true;
-    //
-    //     let msg = PeerMessage(Message::Unchoke);
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(!state.client_is_choked);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_interested_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //     state.peer_is_interested = false;
-    //
-    //     let msg = PeerMessage(Message::Interested);
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(state.peer_is_interested);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_not_interested_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //     state.peer_is_interested = true;
-    //
-    //     let msg = PeerMessage(Message::NotInterested);
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(!state.peer_is_interested);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_have_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //
-    //     let msg = PeerMessage(Message::Have(2));
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(state.peer_bitfield.has_piece(2));
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_bitfield_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     let (picker, mut fp) = prepare_mocks();
-    //
-    //     let msg = PeerMessage(Message::Bitfield(vec![1]));
-    //     let _result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert_eq!(state.peer_bitfield.content, vec![1]);
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_request_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.peer_is_choked = false;
-    //     state.peer_is_interested = true;
-    //     state.client_bitfield.piece_acquired(0);
-    //     let (picker, mut fp) = prepare_mocks();
-    //
-    //     let msg = PeerMessage(Message::Request(Block::new(0, 0, config::BLOCK_SIZE_BYTES)));
-    //     let result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(result.messages_for_peer.iter().any(|msg| msg.is_piece()));
-    // }
-    //
-    // #[tokio::test]
-    // async fn handle_piece_message_test() {
-    //     let mut state = P2PState::new(0, Bitfield::init(5), 5);
-    //     state.peer_is_choked = false;
-    //     state.peer_is_interested = true;
-    //     state.client_bitfield.piece_acquired(0);
-    //     state.ongoing_requests.insert(Block::new(0, 0, 0));
-    //     let (picker, mut fp) = prepare_mocks();
-    //
-    //     let msg = PeerMessage(Message::Piece(DataBlock::new(0, 0, vec![])));
-    //     let result = handle(msg, &mut state, &mut fp, &picker).await;
-    //
-    //     assert!(state.ongoing_requests.is_empty());
-    //     assert!(result.internal_events.iter().any(|msg| msg.is_block_downloaded()));
-    // }
-    //
-    // fn prepare_mocks() -> (Arc<Mutex<dyn PiecePicker>>, Box<dyn FileProv>) {
-    //     let mut picker = MockPiecePicker::new();
-    //     picker.expect_increase_availability_for_pieces().returning(|_| ());
-    //     picker.expect_pick().returning(move |_, _| None);
-    //     picker.expect_increase_availability_for_piece().returning(|_| ());
-    //
-    //     let mut fp = MockFileProv::new();
-    //     fp.expect_read_block().returning(|_| vec![]);
-    //
-    //     return (Arc::new(Mutex::new(picker)), Box::new(fp));
-    // }
+        pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
+
+        assert!(result.internal_events.is_empty());
+        assert_eq!(state.ongoing_requests.len(), picked.len());
+        assert_eq!(result.messages_for_peer.len(), picked.len());
+        assert!(result.messages_for_peer.iter().all(|msg| msg.is_request()));
+    }
+
+    #[tokio::test]
+    async fn pick_blocks_when_client_choked_test() {
+        let mut result = HandlerResult::new();
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        state.client_is_choked = true;
+        state.client_is_interested = true;
+        let initial_state = state.clone();
+        let picker = Arc::new(Mutex::new(MockPiecePicker::new()));
+
+        pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
+
+        assert!(result.internal_events.is_empty());
+        assert!(result.messages_for_peer.is_empty());
+        assert_eq!(state, initial_state);
+    }
+
+    #[tokio::test]
+    async fn pick_blocks_when_client_not_interested_test() {
+        let mut result = HandlerResult::new();
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        state.client_is_choked = false;
+        state.client_is_interested = false;
+        let initial_state = state.clone();
+        let picker = Arc::new(Mutex::new(MockPiecePicker::new()));
+
+        pick_blocks(&mut state, &mut result, &(picker as Arc<Mutex<dyn PiecePicker>>)).await;
+
+        assert!(result.internal_events.is_empty());
+        assert!(result.messages_for_peer.is_empty());
+        assert_eq!(state, initial_state);
+    }
+
+    #[tokio::test]
+    async fn handle_choke_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+        state.client_is_choked = false;
+
+        let msg = PeerMessage(Message::Choke);
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(state.client_is_choked);
+    }
+
+    #[tokio::test]
+    async fn handle_unchoke_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+        state.client_is_choked = true;
+
+        let msg = PeerMessage(Message::Unchoke);
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(!state.client_is_choked);
+    }
+
+    #[tokio::test]
+    async fn handle_interested_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+        state.peer_is_interested = false;
+
+        let msg = PeerMessage(Message::Interested);
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(state.peer_is_interested);
+    }
+
+    #[tokio::test]
+    async fn handle_not_interested_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+        state.peer_is_interested = true;
+
+        let msg = PeerMessage(Message::NotInterested);
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(!state.peer_is_interested);
+    }
+
+    #[tokio::test]
+    async fn handle_have_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+
+        let msg = PeerMessage(Message::Have(2));
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(state.peer_bitfield.has_piece(2));
+    }
+
+    #[tokio::test]
+    async fn handle_bitfield_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        let (picker, mut fp) = prepare_mocks();
+
+        let msg = PeerMessage(Message::Bitfield(vec![1]));
+        let _result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert_eq!(state.peer_bitfield.content, vec![1]);
+    }
+
+    #[tokio::test]
+    async fn handle_request_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        state.peer_is_choked = false;
+        state.peer_is_interested = true;
+        state.client_bitfield.piece_acquired(0);
+        let (picker, mut fp) = prepare_mocks();
+
+        let msg = PeerMessage(Message::Request(Block::new(0, 0, config::BLOCK_SIZE_BYTES)));
+        let result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(result.messages_for_peer.iter().any(|msg| msg.is_piece()));
+    }
+
+    #[tokio::test]
+    async fn handle_piece_message_test() {
+        let mut state = P2PState::new(0, Bitfield::init(5), 5);
+        state.peer_is_choked = false;
+        state.peer_is_interested = true;
+        state.client_bitfield.piece_acquired(0);
+        state.ongoing_requests.insert(Block::new(0, 0, 0));
+        let (picker, mut fp) = prepare_mocks();
+
+        let msg = PeerMessage(Message::Piece(DataBlock::new(0, 0, vec![])));
+        let result = handle(msg, &mut state, &mut fp, &picker).await;
+
+        assert!(state.ongoing_requests.is_empty());
+        assert!(result.internal_events.iter().any(|msg| msg.is_block_downloaded()));
+    }
+
+    fn prepare_mocks() -> (Arc<Mutex<dyn PiecePicker>>, Box<dyn FileProv>) {
+        let mut picker = MockPiecePicker::new();
+        picker.expect_increase_availability_for_pieces().returning(|_| ());
+        picker.expect_pick().returning(move |_, _| vec![]);
+        picker.expect_increase_availability_for_piece().returning(|_| ());
+
+        let mut fp = MockFileProv::new();
+        fp.expect_read_block().returning(|_| vec![]);
+
+        return (Arc::new(Mutex::new(picker)), Box::new(fp));
+    }
 }
